d70d7ef8ef3e0166e3241a25aa8420aea1b08f27
Carlos Delgado <carlos.delgado@reigndesign.com>
2019-07-21 13:45:12 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const dust = require("dustjs-helpers");
+const consolidate = require("consolidate");
+const { Client } = require("pg");
+
+const app = express();
+
+const conectionStr = "postgres://gorydev:Darkgo13@localhost/nodetest";
+
+app.engine("dust", consolidate.dust);
+app.set("view engine", "dust");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  const client = new Client({
+    connectionString: conectionStr
+  });
+  client.connect();
+  client.query("SELECT * FROM recipes", (err, resp) => {
+    if (err) console.log(err);
+    if (resp) {
+      console.log(resp.rows);
+    }
+
+    client.end();
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
